0d5619b792a0ee1f929070b1b9af9b684b7a6302
aSwanting <gab.damico@gmail.com>
2023-10-03 17:02:16 +0200

    add: bootstrap style, btn triggers for user input

--- js/script.js
+++ js/script.js
@@ -5,8 +5,12 @@ let firstName
 
 //- get user input for 'name'
 
-firstName = prompt('Enter your first name')
-console.log('firstName = ' + firstName)
+function getFirstName() {
+    firstName = prompt('Enter your first name')
+    console.log('firstName = ' + firstName)
+    const firstNameDOM = document.getElementById("firstNameDOM")
+    firstNameDOM.innerHTML = firstName
+}
 
 //#### Get user lastName
 //- declare variable 'lastName'
@@ -15,8 +19,12 @@ let lastName
 
 //- get user input for 'lastName'
 
-lastName = prompt('Enter your last name')
-console.log('lastName = ' + lastName)
+function getLastName() {
+    lastName = prompt('Enter your last name')
+    console.log('lastName = ' + lastName)
+    const lastNameDOM = document.getElementById("lastNameDOM")
+    lastNameDOM.innerHTML = lastName
+}
 
 //#### Get user favourite colour
 //- declare variable 'favColour'
@@ -25,12 +33,16 @@ let favColour
 
 //- get user input for 'favColour'
 
-favColour = prompt('Enter your favourite colour')
-console.log('favColour = ' + favColour)
+function getFavColour() {
+    favColour = prompt('Enter your favourite colour')
+    console.log('favColour = ' + favColour)
+    const favColourDOM = document.getElementById("favColourDOM")
+    favColourDOM.innerHTML = favColour
+}
 
 // - Generate random number between 1 and 21
 
-function getRandomIntInclusive(min, max) {
+function genRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -38,16 +50,29 @@ function getRandomIntInclusive(min, max) {
 
 // - Assign random number to variable 'randomNumber'
 
-let randomNumber = getRandomIntInclusive(1, 21)
-console.log('radomNumber = ' + randomNumber)
+let randomNumber
+
+function getRandomNumber() {
+    randomNumber = genRandomIntInclusive(1, 21)
+    console.log('radomNumber = ' + randomNumber)
+    const randomNumberDOM = document.getElementById("randomNumberDOM")
+    randomNumberDOM.innerHTML = randomNumber
+}
+
 
 //#### Output 'name'+'lastName'+'favColour'+'randomNumber'
 //- declare variable 'password' as 'name'+'lastName'+'favColour'+'randomNumber'
 
-let password = firstName + lastName + favColour + randomNumber
+let password
 
 //- Print output of 'password' on page
 
-alert('Your new password is = ' + password)
-console.log('password = ' + password)
+function generatePassword() {
+    password = firstName + lastName + favColour + randomNumber
+    /* alert('Your new password is = ' + password) */
+    console.log('password = ' + password)
+    const passwordDOM = document.getElementById("passwordDOM")
+    passwordDOM.innerHTML = password
+}
+
 
