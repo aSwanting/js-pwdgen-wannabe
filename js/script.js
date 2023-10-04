9205352452be841675a0b68525317ee1492e647e
aSwanting <gab.damico@gmail.com>
2023-10-04 11:43:06 +0200

    refactor: removed global variables, less variables

--- js/script.js
+++ js/script.js
@@ -1,67 +1,59 @@
-//#### Get user name
-//- declare variable 'name'
 
-let firstName
-
-//- get user input for 'name'
+// get user input for 'firstName'
 
 function getFirstName() {
-    firstName = prompt('Enter your first name')
-    console.log('firstName = ' + firstName)
-    const firstNameDOM = document.getElementById("firstNameDOM")
-    firstNameDOM.innerHTML = firstName
-    if (firstName) {
-        const firstNameButton = document.getElementById("firstNameButton")
-        firstNameButton.classList.remove("border-danger")
+    const firstName = prompt('Enter your first name')
+    document.getElementById("firstNameDOM").innerHTML = firstName
+}
+
+// check the value of 'firstName'
+
+function checkFirstName() {
+    if (document.getElementById("firstNameDOM").innerHTML) {
+        document.getElementById("firstNameButton").classList.remove("border-danger")
+        return (document.getElementById("firstNameDOM").innerHTML)
     } else {
-        const firstNameButton = document.getElementById("firstNameButton")
-        firstNameButton.classList.add("border-danger")
+        document.getElementById("firstNameButton").classList.add("border-danger")
     }
 }
 
-//#### Get user lastName
-//- declare variable 'lastName'
-
-let lastName
-
-//- get user input for 'lastName'
+// get user input for 'lastName'
 
 function getLastName() {
-    lastName = prompt('Enter your last name')
-    console.log('lastName = ' + lastName)
-    const lastNameDOM = document.getElementById("lastNameDOM")
-    lastNameDOM.innerHTML = lastName
-    if (lastName) {
-        const lastNameButton = document.getElementById("lastNameButton")
-        lastNameButton.classList.remove("border-danger")
+    const lastName = prompt('Enter your last name')
+    document.getElementById("lastNameDOM").innerHTML = lastName
+}
+
+// check the value of 'lastName'
+
+function checkLastName() {
+    if (document.getElementById("lastNameDOM").innerHTML) {
+        document.getElementById("lastNameButton").classList.remove("border-danger")
+        return (document.getElementById("lastNameDOM").innerHTML)
     } else {
-        const lastNameButton = document.getElementById("lastNameButton")
-        lastNameButton.classList.add("border-danger")
+        document.getElementById("lastNameButton").classList.add("border-danger")
     }
 }
 
-//#### Get user favourite colour
-//- declare variable 'favColour'
-
-let favColour
-
-//- get user input for 'favColour'
+// get user input for 'favColour'
 
 function getFavColour() {
-    favColour = prompt('Enter your favourite colour')
-    console.log('favColour = ' + favColour)
-    const favColourDOM = document.getElementById("favColourDOM")
-    favColourDOM.innerHTML = favColour
-    if (favColour) {
-        const favColourButton = document.getElementById("favColourButton")
-        favColourButton.classList.remove("border-danger")
+    const favColour = prompt('Enter your favourite colour')
+    document.getElementById("favColourDOM").innerHTML = favColour
+}
+
+// check the value of 'favColour'
+
+function checkFavColour() {
+    if (document.getElementById("favColourDOM").innerHTML) {
+        document.getElementById("favColourButton").classList.remove("border-danger")
+        return (document.getElementById("favColourDOM").innerHTML)
     } else {
-        const favColourButton = document.getElementById("favColourButton")
-        favColourButton.classList.add("border-danger")
+        document.getElementById("favColourButton").classList.add("border-danger")
     }
 }
 
-// - Generate random number between 1 and 21
+// generate random number between 1 and 21
 
 function genRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -69,58 +61,50 @@ function genRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// - Assign random number to variable 'randomNumber'
-
-let randomNumber
-
 function getRandomNumber() {
-    randomNumber = genRandomIntInclusive(1, 21)
-    console.log('radomNumber = ' + randomNumber)
-    const randomNumberDOM = document.getElementById("randomNumberDOM")
-    randomNumberDOM.innerHTML = randomNumber
-    if (randomNumber) {
-        const randomNumberButton = document.getElementById("randomNumberButton")
-        randomNumberButton.classList.remove("border-danger")
+    document.getElementById("randomNumberDOM").innerHTML = genRandomIntInclusive(1, 21)
+}
+
+// check the value of 'randomNumber'
+
+function checkRandomNumber() {
+    if (document.getElementById("randomNumberDOM").innerHTML) {
+        document.getElementById("randomNumberButton").classList.remove("border-danger")
+        return (document.getElementById("randomNumberDOM").innerHTML)
     } else {
-        const randomNumberButton = document.getElementById("randomNumberButton")
-        randomNumberButton.classList.add("border-danger")
+        document.getElementById("randomNumberButton").classList.add("border-danger")
     }
 }
 
-//#### Output 'name'+'lastName'+'favColour'+'randomNumber'
-//- declare variable 'password' as 'name'+'lastName'+'favColour'+'randomNumber'
-
-let password
-
-//- Print output of 'password' on page
+// check that all fields have been completed
 
 function generatePassword() {
 
-    if (!firstName) {
-        const firstNameButton = document.getElementById("firstNameButton")
-        firstNameButton.classList.add("border-danger")
+    if (!checkFirstName()) {
+        document.getElementById("firstNameButton").classList.add("border-danger")
     }
 
-    if (!lastName) {
-        const lastNameButton = document.getElementById("lastNameButton")
-        lastNameButton.classList.add("border-danger")
+    if (!checkLastName()) {
+        document.getElementById("lastNameButton").classList.add("border-danger")
     }
 
-    if (!favColour) {
-        const favColourButton = document.getElementById("favColourButton")
-        favColourButton.classList.add("border-danger")
+    if (!checkFavColour()) {
+        document.getElementById("favColourButton").classList.add("border-danger")
     }
 
-    if (!randomNumber) {
-        const randomNumberButton = document.getElementById("randomNumberButton")
-        randomNumberButton.classList.add("border-danger")
+    if (!checkRandomNumber()) {
+        document.getElementById("randomNumberButton").classList.add("border-danger")
     }
 
-    if (firstName && lastName && favColour && randomNumber) {
-        password = firstName + lastName + favColour + randomNumber
-        console.log('password = ' + password)
-        const passwordDOM = document.getElementById("passwordDOM")
-        passwordDOM.innerHTML = password
+    if (checkFirstName() && checkLastName() && checkFavColour() && checkRandomNumber()) {
+        password = checkFirstName() + checkLastName() + checkFavColour() + checkRandomNumber()
+        document.getElementById("passwordDOM").classList.remove("text-danger")
+        document.getElementById("passwordDOM").innerHTML = password
+    }
+
+    else {        
+        document.getElementById("passwordDOM").classList.add("text-danger")
+        document.getElementById("passwordDOM").innerHTML = "(╯°□°)╯︵ ┻━┻"
     }
 
 }
